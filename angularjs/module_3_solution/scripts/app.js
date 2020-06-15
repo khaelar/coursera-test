aa301250380155a4396c7fa955d3a7e1750755a7
khaelar <midavmidav2000@gmail.com>
2020-06-15 21:52:18 +0300

    Added module 3 solution vO.1

---
+++ angularjs/module_3_solution/scripts/app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+(function(){
+	angular.module("NarrowItDownApp", [])
+	.controller("NarrowItDownController", NarrowItDownController)
+	.service("MenuSearchService", MenuSearchService)
+	.directive("foundItems", FoundItems);
+	
+	NarrowItDownController.$inject = ["MenuSearchService"];
+	function NarrowItDownController(MenuSearchService){
+		let ctrl = this;
+		ctrl.found = []
+		
+		ctrl.btclick = function(){
+			MenuSearchService.getMatchedMenuItems(ctrl.searchStr).then(function(result){ctrl.found = result; console.log(ctrl.found)});
+		}
+		
+		ctrl.removeItem = function(itemIndex){
+			ctrl.found.splice(itemIndex, 1);
+		}
+	}
+	
+	MenuSearchService.$inject = ["$http"];
+	function MenuSearchService($http){
+		let service = this;
+		
+		service.getMatchedMenuItems = function(searchTerm){
+			return $http({url: "https://davids-restaurant.herokuapp.com/menu_items.json"})
+			.then(function(result){
+				let foundItems = result.data.menu_items.filter(a => ~a.description.indexOf(searchTerm));
+				return foundItems;
+			});
+		}
+	}
+	
+	function FoundItems(){
+		let ddo = {
+			templateUrl: "foundItems.html",
+			scope: {
+				foundItems: '<',
+				onRemove: '&'
+			}
+		};
+		
+		return ddo;
+	}
+})()
