631b12b1ab117cf09c8510a196f199bc87e251a9
khaelar <midavmidav2000@gmail.com>
2020-06-18 19:42:26 +0300

    SignupController updates

--- angularjs/module_5_solution/src/public/signup/signupController.js
+++ angularjs/module_5_solution/src/public/signup/signupController.js
@@ -19,18 +19,20 @@
           email: signupCtrl.user.email,
           favitemname: signupCtrl.user.favitemname,
     }
-    console.log(userList);
+    
     MenuService.getMenuItem(signupCtrl.user.favitemname)
       .then(function(data) {
-        console.log("Dish found:", data);
+        console.log("Item found:", data);
         userList.favoriteitemname = data;
         userService.addItem(userList);
         $ctrl.favoriteDishFound = true;
+		signupCtrl.msg = "Your information has been saved";
       },
       function(err) {
-        console.log("Dish not found");
+        console.log("Item not found");
         userService.addItem(userList);
         $ctrl.favoriteDishFound = false;
+		signupCtrl.msg = "No such menu number exists";
       });
   };
 
